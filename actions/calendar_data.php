13c7d047f3b41a61ce8161b25418d6b7da43ff80
rhode <vanwykrhode30@gmail.com>
2026-06-01 23:03:47 +0200

    remove error and success messages

--- actions/calendar_data.php
+++ actions/calendar_data.php
@@ -2,27 +2,7 @@
 
 require_once("../database/db_connection.php");
 
-$success_msg = "";
-$error_msg = "";
 $events_from_db = [];
-
-if (isset($_GET["success"])) {
-  $success_msg = match ($_GET["success"]) {
-    "1" => "Your Event Was Deleted Succesfully!",
-    "2" => "Your Event Was Updated Succesfully!",
-    "3" => "Your Event Was Deleted Succesfully!",
-    default => ""
-  };
-}
-
-if (isset($_GET["error"])) {
-  $error_msg = match ($_GET["error"]) {
-    "1" => "ERROR: Your Event Was NOT Added",
-    "2" => "ERROR: Your Event Was NOT Updated",
-    "3" => "ERROR: Your Event Was NOT Deleted",
-    default => ""
-  };
-}
 
 $result = $conn->query("
     SELECT * FROM events
